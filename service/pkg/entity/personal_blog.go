7f3d8dbdde463e5a8d9d206b1ef45ddc5c7ff3a8
wonpanu <won.panumas@gmail.com>
2022-03-28 07:00:08 +0700

    add user id and author field for blog

--- service/pkg/entity/personal_blog.go
+++ service/pkg/entity/personal_blog.go
@@ -4,6 +4,8 @@ import "time"
 
 type Blog struct {
 	ID        string    `json:"id" bson:"_id"`
+	UserID    string    `json:"user_id" bson:"user_id"`
+	Author    string    `json:"author" bson:"author"`
 	Title     string    `json:"title" bson:"title"`
 	Content   string    `json:"content" bson:"content"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
