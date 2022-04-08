22f56e635414aa4cdcec4d1f628fed156c0fe5b4
wonpanu <won.panumas@gmail.com>
2022-04-08 08:36:57 +0700

    mock data

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,10 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import Profile from "../components/profile";
 
-const name = "wonpanu";
 export const siteTitle = "wonpanu";
 
 export default function Layout({ children, home }) {
@@ -22,49 +20,23 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
+      <div className="block">
+        <div className="block m-auto max-w-screen-2xl">
+          <div className="flex flex-direction-row justify-between">
+            <Profile />
+            <main className="block pb-0 flex-grow flex-shrink min-w-0">
+              <main>{children}</main>
+              {!home && (
+                <div className={styles.backToHome}>
+                  <Link href="/">
+                    <a>← Back to home</a>
+                  </Link>
+                </div>
+              )}
+            </main>
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
